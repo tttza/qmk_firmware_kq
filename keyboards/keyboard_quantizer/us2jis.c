4ae28f8d7d01455ddff47fa85700677b27f9b1a4
tttza <25410554+tttza@users.noreply.github.com>
2021-05-09 20:28:49 +0900

    us2jis

--- keyboards/keyboard_quantizer/us2jis.c
+++ keyboards/keyboard_quantizer/us2jis.c
@@ -1,4 +1,4 @@
-/* Copyright 2020 sekigon-gonnoc
+/* Copyright 2018-2020 eswai <@eswai>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,15 +13,16 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+/*
+  OSで日本語キーボード(logical bit pairing)と設定/認識されているキーボードで、
+  USキーキャップの文字、記号(typewriter pairing)を正しく出力する。
+  例: Shift + 2 で @ を入力する
+  変換された文字はキーリピートが無効です。
+*/
+
 #include QMK_KEYBOARD_H
 #include "keymap_jp.h"
-
-// Defines names for use in layer keycodes and the keymap
-enum layer_names {
-    _BASE,
-    _LOWER,
-    _RAISE,
-};
 
 const uint16_t us2jis[][2] = {
   {KC_LPRN, JP_LPRN},
@@ -48,31 +49,7 @@ const uint16_t us2jis[][2] = {
   {KC_CIRC, JP_CIRC},
 };
 
-// Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes {
-    CUSTOM_KEYCODE1 = SAFE_RANGE,
-};
-
-
-#define LOSP LT(_LOWER, KC_SPC)
-#define RAEN LT(_RAISE, KC_ENT)
-
-// clang-format off
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Base */
-    [_BASE] =  LAYOUT(
-    KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4,     KC_F5, KC_F6, KC_F7, KC_F8,     KC_F9, KC_F10, KC_F11, KC_F12,    KC_PSCR, KC_SCROLLLOCK, KC_PAUS,
-        JP_ZHTG, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, JP_MINS, JP_CIRC, JP_YEN, KC_BSPACE, KC_INSERT, KC_HOME, KC_PGUP,       KC_NUMLOCK, KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS,
-        KC_TAB,   KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, JP_AT, JP_LBRC,           KC_ENT,   KC_DEL, KC_END,  KC_PGDN,          KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_PLUS,
-        KC_CAPS,   KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, JP_SCLN, JP_COLN, JP_RBRC,                                                  KC_KP_4, KC_KP_5, KC_KP_6,
-        KC_LSFT,    KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, JP_SLSH, JP_BSLS, KC_RSFT,                 KC_UP,                     KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_ENTER,
-        KC_LCTRL, KC_LGUI, KC_LALT, JP_MHEN, LOSP,  RAEN, JP_KANA, KC_RALT, KC_RGUI, KC_APP, KC_RCTRL,            KC_LEFT,  KC_DOWN, KC_RIGHT,       KC_KP_0,          KC_KP_DOT
-    )
-};
-// clang-format on
-\\
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-
+bool twpair_on_jis(uint16_t keycode, keyrecord_t *record) {
   if (!record->event.pressed) return true;
 
   uint16_t skeycode; // シフトビットを反映したキーコード
@@ -103,15 +80,5 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
   }
 
-    return true;
+  return true;
 }
-
-#ifdef OLED_DRIVER_ENABLE
-#    include "rev1.h"
-#    include "oled_driver.h"
-#endif
-
-#ifdef OLED_DRIVER_ENABLE
-void oled_task_user(void) { render_logo(); }
-#endif
-
